7e25fea1c00c298355fbc58528d307816d01c261
Alan Estrada <joshualan23@yahoo.com>
2016-04-11 11:17:51 -0400

    Update example2.py

--- TempTables/example2.py
+++ TempTables/example2.py
@@ -1,8 +1,12 @@
+# Why ABL Example
+# Authors: Bill Wood, Alan Estrada
+# File Name: BasicQuery/example2.py
+ 
 import MySQLdb as mdb
 
 class TempTable:
     customerNumber = 0
-    salesRep = 0
+    email = 0
 
 with open('input.txt', 'r') as f:
     lines = f.readlines()
@@ -15,20 +19,29 @@ for line in lines:
     tt = TempTable()
     
     tt.customerNumber = int(words[0])
-    tt.salesRep = int(words[1])
+    tt.email = int(words[1])
 
     temptable.append(tt)
 
 try:
-    
     db = mdb.connect('localhost', 'root', '', 'classicmodels')
     cur = db.cursor()
 
+    # After being given a list of customers and the emails of their new sales reps,
+    # iterate through each customer and assign them the sales rep that the email
+    # belongs to.
+    #
+    # This is analagous to the ABL code:
+    # FOR EACH tt:
+    #    FIND Customers WHERE Customers.customerNumber = tt.customerNumber.
+    #    FIND employees WHERE employees.email = tt.email.
+    #    Customers.salesRepEmployeeNumber = employees.employeeNumber.
+    # END.
     for tt in temptable:
         cur.execute("UPDATE customers " +  \
                     "SET salesRepEmployeeNumber = " + \
-                    "(SELECT employeeNumber FROM employees WHERE employeeNumber = {}) ".format(tt.salesRep) + \
-                    "WHERE customerNumber = {}".format(tt.salesRep))
+                    "(SELECT employeeNumber FROM employees WHERE email = {}) ".format(tt.email) + \
+                    "WHERE customerNumber = {}".format(tt.customerNumber))
 
     db.commit()
     print "{} rows updated".format(cur.rowcount)
