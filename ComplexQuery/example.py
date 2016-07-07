d281d628810fdf160c3adeccadb24531c0aeb59c
Alan Estrada <joshualan23@yahoo.com>
2016-07-06 20:30:52 -0400

    Updated code to be more idiomatic.

--- ComplexQuery/example.py
+++ ComplexQuery/example.py
@@ -1,32 +1,33 @@
 # Why ABL Example
 # Authors: Bill Wood, Alan Estrada
 # File Name: ComplexQuery/example.py
-# Version 1.04
-# 
+# Version 1.05
+#
 # This is the Python equivalent of ComplexQuery/example.p
 
-import MySQLdb as mdb
+import MySQLdb
 
+# We might potentially get this from a file or user input
 shipDate = '1999-09-19'
 
 try:
-    con = mdb.connect('localhost', 'root', '', 'sports2000')
-    cur = con.cursor(mdb.cursors.DictCursor)
+    con = MySQLdb.connect('localhost', 'root', '', 'sports2000')
+    cur = con.cursor(MySQLdb.cursors.DictCursor)
 
     # Join on 4 tables
     # We do an join the Employees, Customers, Orders, and OrderDetails tables
-    # and then sort out all the orders where we are processing orders and the order
-    # contains a specific product
-    sql = "SELECT * FROM salesrep JOIN customer " + \
-        "ON customer.salesRep = salesrep.salesrep " + \
-        "JOIN order ON order.custNum = customer.custNum " + \
-        "JOIN orderline ON orderline.ordernum = order.orderNum " + \
-        "WHERE order.OrderStatus = 'Ordered' OR " + \
-        "(order.orderStatus = 'Shipped' AND " + \
-        "ABS(DATEDIFF(order.shipDate, {})) <= 31) ".format(shipDate) + \
-        "AND orderline.itemnum = 14;"
+    # and then sort out all the orders where we are processing orders and the 
+    # order contains a specific product
+    sql = ("SELECT * FROM salesrep JOIN customer "
+           "ON customer.salesRep = salesrep.salesrep "
+           "JOIN order ON order.custNum = customer.custNum "
+           "JOIN orderline ON orderline.ordernum = order.orderNum "
+           "WHERE order.OrderStatus = 'Ordered' OR "
+           "(order.orderStatus = 'Shipped' AND "
+           "ABS(DATEDIFF(order.shipDate, %s)) <= 31) "
+           "AND orderline.itemnum = 14;")
 
-    cur.execute(sql)
+    cur.execute(sql, (shipDate,))
 
     rows = cur.fetchall()
 
