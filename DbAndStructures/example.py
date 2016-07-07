53f12aba5e08510d0e771738dedf9fe4d0117efb
Alan Estrada <joshualan23@yahoo.com>
2016-07-06 20:31:09 -0400

    Updated code to be more idiomatic.

--- DbAndStructures/example.py
+++ DbAndStructures/example.py
@@ -1,13 +1,9 @@
 # Why ABL Example
 # Authors: Bill Wood, Alan Estrada
 # File Name: DbAndStructures/example.py
-# Version 1.04
+# Version 1.05
 
-import MySQLdb as mdb
-
-class PrimeCustomer:
-    id = 0
-    newRep = ""
+import MySQLdb
 
 with open('input.txt', 'r') as f:
     lines = f.readlines()
@@ -16,36 +12,28 @@ primeCustomers = []
 
 for line in lines:
     words = line.split()
-
-    tt = PrimeCustomer()
-    
-    tt.id = int(words[0])
-    tt.newRep = words[1]
-
-    primeCustomers.append(tt)
+    primeCustomers.append({"id": int(words[0]), "newRep": words[1]})
 
 try:
-    db = mdb.connect('localhost', 'root', '', 'sports2000')
+    db = MySQLdb.connect('localhost', 'root', '', 'sports2000')
     cur = db.cursor()
 
     # Get a list of all the customers we are going to impact
     # This section is analagous to the ABL code:
-    # FOR EACH primeCustomer, FIRST Customer WHERE Customer.CustNum = primeCustomer.id BY primeCustomer.id:
+	# FOR EACH primeCustomer, FIRST Customer
+    #   WHERE Customer.CustNum = primeCustomer.id BY primeCustomer.id:
     #   DISPLAY Customer.Name primeCustomer.id.
     # END.
-    
+
     # Create a string to use for the SQL IN operator
     customerNumbers = '('
-    for prime in primeCustomers:
-        customerNumbers += str(prime.id)
-        customerNumbers += ', '
-        
-    # Remove the trailing comma and space and add ending parenthesis 
-    customerNumbers = customerNumbers[:-2]
+    customerNumbers += ", ".join([p['id'] for p in primeCustomers])
     customerNumbers += ')'
 
-    cur.execute("SELECT Name, custNum from customers where custNum in {}".format(customerNumbers))
-    
+    sql = ('SELECT Name, custNum FROM customers'
+           ' WHERE custNum IN %s')
+    cur.execute(sql, (customerNumbers,))
+
     row = cur.fetchone()
     while row is not None:
         print ", ".join([str(c) for c in row])
@@ -53,19 +41,21 @@ try:
 
     # Get a list of all the new salesReps for Massachusetts customers
     # This is analagous to the ABL code:
-    # FOR EACH Customer WHERE Customer.State = "MA", EACH primeCustomer WHERE primeCustomer.id = Customer.CustNum:
+    # FOR EACH Customer WHERE Customer.State = "MA",
+    #    EACH primeCustomer WHERE primeCustomer.id = Customer.CustNum:
     #    DISPLAY Customer.Name primeCustomer.newRep.
     # END.
-    cur.execute("SELECT Name, custNum from customers where State = 'MA'")
-    
+    sql = "SELECT Name, custNum from customers where State = 'MA'"
+    cur.execute(sql)
+
     row = cur.fetchone()
     while row is not None:
-	for prime in primeCustomers:
-		if row[1] == prime.id:
-			print row[0], prime.newRep
-		
+        for p in primeCustomers:
+            if row[1] == p['id']:
+                print row[0], p['newRep']
+
         row = cur.fetchone()
-		
+
     # Go through each customer and assign them the sales rep
     # with the given name
     #
@@ -75,11 +65,12 @@ try:
     #    FIND SalesRep WHERE SalesRep.RepName = primeCustomer.newRep.
     #    Customer.SalesRep = SalesRep.SalesRep.
     # END.
-    for prime in primeCustomers:
-        cur.execute("UPDATE customer " +  \
-                    "SET salesRep = " + \
-                    "(SELECT salesRep FROM SalesRep WHERE repName = '{}' LIMIT 1) ".format(prime.newRep) + \
-                    "WHERE customerNumber = {}".format(prime.id))
+    for p in primeCustomers:
+        sql = ('UPDATE customer'
+               'SET salesRep = '
+               '(SELECT salesRep FROM SalesRep WHERE repName = "%s" LIMIT 1) '
+               'WHERE customerNumber = %s')
+        cur.execute(sql, (p['newRep'], p['id']))
 
     db.commit()
 except Exception as e:
