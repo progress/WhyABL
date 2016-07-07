31cc5dad00d8cc2f29e18702f52051b6bae2100f
Alan Estrada <joshualan23@yahoo.com>
2016-07-06 20:30:30 -0400

    Updated code to be more idiomatic.

--- BasicQuery/example.py
+++ BasicQuery/example.py
@@ -1,32 +1,33 @@
 # Why ABL Example
 # Authors: Bill Wood, Alan Estrada
 # File Name: BasicQuery/example.py
-# Version 1.04 
+# Version 1.05
 #
 # This is the Python equivalent of this slice of ABL code:
 # FOR EACH Customer WHERE SalesRep = repname AND Balance > CreditLimit:
 #    Balance = Balance * creditFactor.
 # END.
 
-import MySQLdb as mdb
+import MySQLdb
 
 repName = "GPE"
 creditFactor = 1.05
 
 # Python's try block implicitly gives transactional scoping
 try:
-    db = mdb.connect('localhost', 'root', '', 'sports2000')
-    
+    db = MySQLdb.connect('localhost', 'root', '', 'sports2000')
+
     # Create a cursor object to keep track of result set
     cur = db.cursor()
 
+    sql = ('UPDATE customers'
+           'SET BALANCE = BALANCE * %s'
+           'WHERE SALESREPEMPLOYEENUMBER = %s AND BALANCE > CREDITLIMIT')
+
     # Execute the query
-    cur.execute("UPDATE customers \
-                 SET BALANCE = BALANCE * %s \
-                 WHERE SALESREPEMPLOYEENUMBER = %s AND BALANCE > CREDITLIMIT",
-                (creditFactor, repName))
-    
-    # Commit the changes 
+    cur.execute(sql, (creditFactor, repName))
+
+    # Commit the changes
     db.commit()
     print "{} rows updated".format(cur.rowcount)
 except Exception as e:
